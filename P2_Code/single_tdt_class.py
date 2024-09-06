48033cce00dd3004852d325f174362864df1db07
Albert Li <albertli536@gmail.com>
2024-09-06 15:07:49 -0400

    Started Hab Dishab

--- P2_Code/single_tdt_class.py
+++ P2_Code/single_tdt_class.py
@@ -425,47 +425,6 @@ class TDTData:
 
         self.psth_df = result_df
         return result_df
-    
-    def find_area_and_peak(self, behavior_name, peak_startPoint, peak_endPoint):
-        if self.psth_df.empty:
-            raise ValueError("PSTH DataFrame is empty. Compute PSTH first.")
-
-        psth_mean = self.psth_df['mean'].values
-        timestamps = self.psth_df.index.values
-
-        peak_startPoint = np.asarray(peak_startPoint)
-        peak_endPoint = np.asarray(peak_endPoint)
-
-        peak_startPoint = peak_startPoint[~np.isnan(peak_startPoint)]
-        peak_endPoint = peak_endPoint[~np.isnan(peak_endPoint)]
-
-        if len(peak_startPoint) != len(peak_endPoint):
-            raise ValueError('Number of Peak Start Time and Peak End Time are unequal.')
-
-        if np.less_equal(peak_endPoint, peak_startPoint).any():
-            raise ValueError('Peak End Time is lesser than or equal to Peak Start Time.')
-
-        peak_area = OrderedDict()
-
-        if peak_startPoint.shape[0] == 0 or peak_endPoint.shape[0] == 0:
-            peak_area['peak'] = np.nan
-            peak_area['area'] = np.nan
-
-        for i in range(peak_startPoint.shape[0]):
-            startPtForPeak = np.where(timestamps >= peak_startPoint[i])[0]
-            endPtForPeak = np.where(timestamps >= peak_endPoint[i])[0]
-            if len(startPtForPeak) >= 1 and len(endPtForPeak) >= 1:
-                peakPoint_pos = startPtForPeak[0] + np.argmax(psth_mean[startPtForPeak[0]:endPtForPeak[0]])
-                peakPoint_neg = startPtForPeak[0] + np.argmin(psth_mean[startPtForPeak[0]:endPtForPeak[0]])
-                peak_area[f'peak_pos_{i+1}'] = np.amax(psth_mean[peakPoint_pos])
-                peak_area[f'peak_neg_{i+1}'] = np.amin(psth_mean[peakPoint_neg])
-                peak_area[f'area_{i+1}'] = np.trapz(psth_mean[startPtForPeak[0]:endPtForPeak[0]])
-            else:
-                peak_area[f'peak_{i+1}'] = np.nan
-                peak_area[f'area_{i+1}'] = np.nan
-
-        self.peak_area_results[behavior_name] = peak_area
-        return peak_area
 
 
     def plot_psth(self, behavior_name, signal_type='dFF'):
@@ -563,6 +522,9 @@ class TDTData:
         Parameters:
         plot_type (str): The type of plot to generate. Options are 'raw', 'dFF', and 'zscore'.
         '''
+        total_duration = self.timestamps[-1] - self.timestamps[0]  # Total duration of the data
+        num_major_ticks = 10  # Number of major ticks (adjust this as needed)
+
         if plot_type == 'raw':
             if self.DA in self.streams and self.ISOS in self.streams:
                 plt.figure(figsize=(18, 6))
@@ -598,7 +560,10 @@ class TDTData:
 
         plt.xlabel('Seconds')
         ax = plt.gca()
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(10))  # Adjusts the x-axis ticks to show more tick marks
-        ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))   # Adds minor ticks for more granularity
-        plt.grid(True)
+
+        # Dynamically set the number of major ticks
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(total_duration / num_major_ticks))
+
+        # Remove the grid
+        plt.grid(False)
         plt.show()
